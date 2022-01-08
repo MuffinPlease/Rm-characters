e4988d29d763f2abbfb31ded8db52060b5a78204
MuffinPlease <benjamintec3455@gmail.com>
2022-01-07 19:22:27 -0600

    Random data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
-import getData from 'getData';
+const API = "https://rickandmortyapi.com/api/character/";
 
-const setData = async () => {
-    const character = await getData();
+const searchData = async () =>{
+    try{
+        const number = Math.floor(Math.random() * 826 + 1);
 
-    console.log(character.name);
+        const res = await fetch(`${API}/${number}`);
+        const character = await res.json();
+        return character;
+
+    }catch(e){
+        console.log(e);
+        return null;
+    }
+}
+
+
+const setData = async() => {
+    const character = await searchData();
+    const content = document.querySelector('.content');
+
+    // Variables
+    const name = character.name;
+    const img = character.image;
+    const status = character.status;
+    const gender = character.gender;
+    const species = character.species;
+
+    content.innerHTML += 
+    ` <img src="${img}" alt="${name}">
+    <p class="name">${name}</p>
+    <p><b>Status: </b>${status}</p>
+    <p><b>Gender: </b>${gender}</p>
+    <p><b>Species:  </b>${species}</p>`;
 }
 
 setData();
--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,17 +1,5 @@
-const API = "https://rickandmortyapi.com/api/character/";
-const axios = require('axios');
 
 
-const searchData = async () =>{
-    try{
-        const number = Math.floor(Math.random() * 826 + 1);
-        const promise = await axios.get(`${API}/${number}`);
-        const character = promise.data;
-        return character;
+searchData();
 
-    }catch(e){
-        return null;
-    }
-}
-
-export default searchData;
+// export default searchData;
